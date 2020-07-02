73f98c93dcb48cc9e12147a244bff588e6a54292
Danilo Oliveira <danilo@sigalei.com.br>
2020-07-02 01:15:32 -0300

    feat: add conditional rendering example

---
+++ secondClass/conditional-rendering/src/LoginControl.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Greeting, LoginButton, LogoutButton} from './FunctionalComponents';
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+export default LoginControl;
---
+++ secondClass/conditional-rendering/src/WarningBanner.js
@@ -0,0 +1,11 @@
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
