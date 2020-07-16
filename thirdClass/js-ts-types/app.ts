344081c95b20a122f7c52ffc4e7fc20bd59c2ce9
Danilo Oliveira <danilo@sigalei.com.br>
2020-07-15 21:33:40 -0300

    add js ts types

---
+++ thirdClass/js-ts-types/app.ts
@@ -0,0 +1,12 @@
+function add(n1: number, n2: number) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect input!');
+  // }
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
