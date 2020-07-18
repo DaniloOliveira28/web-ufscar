368e3b532385576efb20963f4ef6fb9e1d605265
Danilo Oliveira <danilo@sigalei.com.br>
2020-07-18 09:32:52 -0300

    feat: add search component

---
+++ thirdClass/my-github-app/src/components/ResultBox/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultBox = () => {
+
+  return (
+    <div>
+      ResulBox
+    </div>
+  )
+}
+export default ResultBox;
---
+++ thirdClass/my-github-app/src/components/SearchBox/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const SearchBox = () => {
+
+  return (
+    <div>
+      <FormControl>
+        <InputLabel htmlFor="input-with-icon-adornment">With a start adornment</InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+    </div>
+  )
+}
+export default SearchBox;
