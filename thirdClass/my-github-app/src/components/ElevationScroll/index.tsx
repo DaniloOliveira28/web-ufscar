2b56df24dd467d491ae2d07d96d165b6a2647edd
Danilo Oliveira <danilo@sigalei.com.br>
2020-07-18 09:16:38 -0300

    feat: add navbar component

---
+++ thirdClass/my-github-app/src/components/ElevationScroll/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+type ElevationScrollProps = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+};
+
+const ElevationScroll = (props: ElevationScrollProps) => {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default ElevationScroll;
--- thirdClass/my-github-app/src/components/NavBar/index.tsx
+++ thirdClass/my-github-app/src/components/NavBar/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => {
-  return <div>NavBar</div>
+import ElevationScroll from '../ElevationScroll';
+
+interface NavBarProps {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const NavBar = (props:NavBarProps) => {
+  return (
+    <>
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">Our TERRIFIC github app</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </>
+  )
 }
 
 export default NavBar;
