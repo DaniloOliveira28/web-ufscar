31682c3282283144f6bcf6c6ab43c4b455905b50
Danilo Oliveira <danilo@sigalei.com.br>
2020-07-18 10:47:11 -0300

    feat: add state and empty component to main

--- thirdClass/my-github-app/src/components/Main/index.tsx
+++ thirdClass/my-github-app/src/components/Main/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {Box} from '@material-ui/core';
 import SearchBox from '../SearchBox';
 import ResultBox from '../ResultBox';
+import EmptyResult from '../EmptyResult';
 
 const Main = () => {
+  const [query, setQuery] = useState('');
+
   return (
     <main>
-      <SearchBox />
-      <ResultBox />
+      <Box my={2}>
+        <SearchBox onChange={(value) => {setQuery(value)}} value={query} />
+      </Box>
+      {query && query !== '' ? <ResultBox query={query}/> : <EmptyResult /> }
     </main>
   );
 }
